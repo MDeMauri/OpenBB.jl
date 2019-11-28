d63c9d9e607f253de1251ce2f02a01d1d74a5be3
Wim Van Roy <wim.vr@hotmail.com>
2019-11-28 16:30:54 +0100

    Fix error flat interface.

--- src/branch_and_bound/setup_update_inspect/update_node.jl
+++ src/branch_and_bound/setup_update_inspect/update_node.jl
@@ -10,42 +10,53 @@
 # this function updates a node to the current version
 function update!(node::BBnode,workspace::BBworkspace{T1,T2,T3})::Bool where T1<:Problem where T2<:AbstractWorkspace where T3<:AbstractSharedMemory
 
-    # check that the register is correct
-    @assert node.version <= workspace.updatesRegister.masterVersion
+	# check that the register is correct
+	@assert node.version <= workspace.updatesRegister.masterVersion
 
-    # apply the all the updates necessary
-    for updateID in node.version+1:workspace.updatesRegister.masterVersion
-        if workspace.updatesRegister.updates[updateID](node,workspace.updatesRegister.arguments[updateID]...,workspace) == false # the update can fail if the problem is discovered infeasible
+	# use a boolean array to keep track of the updated variables
+	updatedVars = falses(get_numVariables(workspace))
+
+	# apply the all the updates necessary
+	for updateID in node.version+1:workspace.updatesRegister.masterVersion
+		feasible = workspace.updatesRegister.updates[updateID](node,workspace.updatesRegister.arguments[updateID]...,workspace,updatedVars)
+		if !feasible # the update can fail if the problem is discovered infeasible
 			return false
 		end
-    end
+	end
 
 	# declare the verision of the node to be the same of the master
 	node.version = workspace.updatesRegister.masterVersion
 
-    return true
+	# Preprocess
+	return preprocess!(node, workspace, findall(updatedVars))
 end
 
 
 # adapt the node to the insertion of constraints
 function insert_constraints!(node::BBnode,position::Int64,newBounds::Tuple{Array{Float64,1},Array{Float64,1}},
-							 workspace::BBworkspace{T1,T2,T3})::Bool where T1<:Problem where T2<:AbstractWorkspace where T3<:AbstractSharedMemory
+							 workspace::BBworkspace{T1,T2,T3}, newUpdatedVars::BitArray{1})::Bool where T1<:Problem where T2<:AbstractWorkspace where T3<:AbstractSharedMemory
 	# check input
 	@assert length(newBounds[1]) == length(newBounds[2])
 	# extend bounds
 	splice!(node.cnsDual,position:position-1,zeros(length(newBounds[1])))
-    splice!(node.cnsLoBs,position:position-1,copy(newBounds[1]))
+	splice!(node.cnsLoBs,position:position-1,copy(newBounds[1]))
 	# extend dual solution
 	splice!(node.cnsUpBs,position:position-1,copy(newBounds[2]))
 
-	# preprocessing
-    preprocess_rows!(node, workspace, collect(position:(position + length(newBounds[1]) - 1)))
+	# Updated variables
+	A = get_linearConstraints(workspace.problem.cnsSet)
+
+	for i=position:position+length(newBounds[1])-1
+		newUpdatedVars[A[i,:] .!= 0] .= true
+	end
+
+	return true
 end
 
 # adapt the node to the removal of constraints
 function remove_constraints!(node::BBnode,indices::Array{Int64,1},
-							 workspace::BBworkspace{T1,T2,T3})::Bool where T1<:Problem where T2<:AbstractWorkspace where T3<:AbstractSharedMemory
-    deleteat!(node.cnsDual,indices)
+							 workspace::BBworkspace{T1,T2,T3}, newUpdatedVars::BitArray{1})::Bool where T1<:Problem where T2<:AbstractWorkspace where T3<:AbstractSharedMemory
+	deleteat!(node.cnsDual,indices)
 	deleteat!(node.cnsLoBs,indices)
 	deleteat!(node.cnsUpBs,indices)
 	return true
@@ -53,7 +64,7 @@ end
 
 # changes the constraints order
 function permute_constraints!(node::BBnode,permutation::Array{Int64,1},
-							  workspace::BBworkspace{T1,T2,T3})::Bool where T1<:Problem where T2<:AbstractWorkspace where T3<:AbstractSharedMemory
+								workspace::BBworkspace{T1,T2,T3}, newUpdatedVars::BitArray{1})::Bool where T1<:Problem where T2<:AbstractWorkspace where T3<:AbstractSharedMemory
 	# check input
 	@assert length(permutation) == length(node.cnsDual)
 	# permute the constraints bounds and dual solution
@@ -67,15 +78,15 @@ end
 # changes variable and constraint bounds in the node
 function update_bounds!(node::BBnode,varLoBs::Array{Float64,1},varUpBs::Array{Float64,1},
 						cnsLoBs::Array{Float64,1},cnsUpBs::Array{Float64,1},
-						workspace::BBworkspace{T1,T2,T3})::Bool where T1<:Problem where T2<:AbstractWorkspace where T3<:AbstractSharedMemory
+						workspace::BBworkspace{T1,T2,T3}, newUpdatedVars::BitArray{1})::Bool where T1<:Problem where T2<:AbstractWorkspace where T3<:AbstractSharedMemory
 	# check input
 	@assert length(varLoBs) == 0 || length(varLoBs) == length(node.varLoBs)
 	@assert length(varUpBs) == 0 || length(varUpBs) == length(node.varUpBs)
 	@assert length(cnsLoBs) == 0 || length(cnsLoBs) == length(node.cnsLoBs)
 	@assert length(cnsUpBs) == 0 || length(cnsUpBs) == length(node.cnsUpBs)
 
-    originalLoBs = copy(node.varLoBs)
-    originalUpBs = copy(node.varUpBs)
+	originalLoBs = copy(node.varLoBs)
+	originalUpBs = copy(node.varUpBs)
 
 	# update the bounds
 	if length(varLoBs)>0 @. node.varLoBs = max(node.varLoBs,varLoBs) end
@@ -94,34 +105,37 @@ function update_bounds!(node::BBnode,varLoBs::Array{Float64,1},varUpBs::Array{Fl
 		end
 	end
 
-	# preprocessing
-    newUpdatedVars = findall(x->!x, originalLoBs .== node.varLoBs)
-    newUpdatedVars = unique(union(newUpdatedVars, findall(x->!x, originalUpBs .== node.varUpBs)))
+	# updated variables
+	newUpdatedVars[findall(x->!x, originalLoBs .== node.varLoBs)] .= true
+	newUpdatedVars[findall(x->!x, originalUpBs .== node.varUpBs)] .= true
 
-    return preprocess!(node, workspace, newUpdatedVars)
+	return true
 end
 
 
 # insert a new set of variables in the node data
 function insert_variables!(node::BBnode,position::Int64,newPrimal::Array{Float64,1},newBounds::Tuple{Array{Float64,1},Array{Float64,1}},
-						   workspace::BBworkspace{T1,T2,T3})::Bool where T1<:Problem where T2<:AbstractWorkspace where T3<:AbstractSharedMemory
+							workspace::BBworkspace{T1,T2,T3}, newUpdatedVars::BitArray{1})::Bool where T1<:Problem where T2<:AbstractWorkspace where T3<:AbstractSharedMemory
 	# check input
 	@assert length(newBounds[1]) == length(newBounds[2]) == length(newPrimal)
 	# extend bounds arrays
-    splice!(node.varLoBs,position:position-1,copy(newBounds[1]))
-    splice!(node.varUpBs,position:position-1,copy(newBounds[2]))
+	splice!(node.varLoBs,position:position-1,copy(newBounds[1]))
+	splice!(node.varUpBs,position:position-1,copy(newBounds[2]))
 	# extend primal/dual solutions
 	splice!(node.primal,position:position-1,copy(newPrimal))
 	splice!(node.bndDual,position:position-1,zeros(length(newPrimal)))
+
+	newUpdatedVars[position:position-1] .= true
+
 	return true
 end
 
 
 # rounds the variable bounds (usually because they were marked as integral)
 function round_variable_bounds!(node::BBnode,indices::Array{Int64,1},
-								workspace::BBworkspace{T1,T2,T3})::Bool where T1<:Problem where T2<:AbstractWorkspace where T3<:AbstractSharedMemory
+								workspace::BBworkspace{T1,T2,T3}, newUpdatedVars::BitArray{1})::Bool where T1<:Problem where T2<:AbstractWorkspace where T3<:AbstractSharedMemory
 	# round bounds
-	@. node.varLoBs[indices] =  ceil(node.varLoBs[indices]-workspace.settings.primalTolerance)
+	@. node.varLoBs[indices] = ceil(node.varLoBs[indices]-workspace.settings.primalTolerance)
 	@. node.varUpBs[indices] = floor(node.varUpBs[indices]+workspace.settings.primalTolerance)
 	# center primal solution
 	@. node.primal = min(max(node.primal,node.varLoBs),node.varUpBs)
@@ -132,14 +146,16 @@ function round_variable_bounds!(node::BBnode,indices::Array{Int64,1},
 		end
 	end
 
-	# preprocessing
-    return preprocess!(node, workspace, indices)
+	# denote updated variables
+	newUpdatedVars[indices] .= true
+
+	return true
 end
 
 
 # fix value of the given variables to the given assignment
 function fix_variables!(node::BBnode,indices::Array{Int,1},values::Array{Float64,1},
-						workspace::BBworkspace{T1,T2,T3})::Bool where T1<:Problem where T2<:AbstractWorkspace where T3<:AbstractSharedMemory
+						workspace::BBworkspace{T1,T2,T3}, newUpdatedVars::BitArray{1})::Bool where T1<:Problem where T2<:AbstractWorkspace where T3<:AbstractSharedMemory
 	# check input
 	@assert length(indices) == length(values)
 	# check if the new assignment is feasible and fix values
@@ -152,6 +168,8 @@ function fix_variables!(node::BBnode,indices::Array{Int,1},values::Array{Float64
 		end
 	end
 
-	# preprocessing
-    return preprocess!(node, workspace, indices)
+	# denote updated variables
+	newUpdatedVars[indices] .= true
+
+	return true
 end
