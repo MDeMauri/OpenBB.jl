b0d731a63b9a09730d5116d8f35c6b75b73cd77d
massimo.demauri@kuleuven.be <massimo.demauri@kuleuven.be>
2020-01-08 19:24:25 +0100

    Bugfix

--- src/problem_definition/ObjectiveFunctions/QuadraticObjective.jl
+++ src/problem_definition/ObjectiveFunctions/QuadraticObjective.jl
@@ -3,7 +3,7 @@
 # @Email:  massimo.demauri@gmail.com
 # @Filename: QuadraticObjective.jl
 # @Last modified by:   massimo
-# @Last modified time: 2020-01-08T19:18:57+01:00
+# @Last modified time: 2020-01-08T19:23:49+01:00
 # @License: LGPL-3.0
 # @Copyright: {{copyright}}
 
@@ -128,5 +128,5 @@ end
 # evaluate the hessian of the objective function in the given point
 function evaluate_hessian(objective::QuadraticObjective{T1,T2},point::Array{Float64,1})::AbstractArray where T1<:Union{Array{Float64,2},SparseMatrixCSC{Float64,Int}}  where T2<:Union{Array{Float64,1},SparseVector{Float64,Int}}
     @assert length(point) == size(objective.Q,2)
-    return objective.Q
+    return copy(objective.Q)
 end
